957e79113a16a222c645b41ec0f7b8321a99ac3f
Jeremy  Dutton <jeremyduttonmacbook@Jeremys-MBP.home>
2023-08-31 22:26:35 -0400

    parametized

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -27,18 +27,21 @@ ORDER BY teacher;
 */
 
 /* CORRECT ANSWER */
-
+const cohort = process.argv[2]
+values = [`%${cohort}%`]
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
 });
+
+
